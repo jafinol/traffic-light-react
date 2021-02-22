1baacd1800155de56ffafaa4fa98e8cbf8d26e92
jafinol <jafinol@gmail.com>
2021-02-22 03:16:21 +0000

    first commit

---
+++ src/js/component/circulo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Circulo = () => {
+	return <div className="circleY"></div>;
+};
+
+export default Circulo;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,73 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
+
 export function Home() {
+	const [clase1, setClase1] = useState("circleR");
+	const [clase2, setClase2] = useState("circleY");
+	const [clase3, setClase3] = useState("circleG");
+	const [time, setTime] = useState(0);
+	const [time2, setTime2] = useState(0);
+	const [id, setId] = useState(0);
+	const [isPause, setPause] = useState(true);
+	const [time3, setTime3] = useState(0);
+	const [time4, setTime4] = useState(0);
+	let c = 0;
+
+	useEffect(() => {
+		setInterval(() => {
+			setTime(time => time + 1);
+			setTime2(time2 => time2 + 1);
+			setTime3(time3 => time3 + 1);
+		}, 1000);
+	}, []);
+
+	if (time === 2) {
+		setTime(0);
+		setClase1("circleRON");
+		setClase2("circleY");
+		setClase3("circleG");
+	}
+
+	if (time2 === 4) {
+		setTime2(0);
+		setTime(0);
+		setClase1("circleR");
+		setClase2("circleYON");
+		setClase3("circleG");
+	}
+
+	if (time3 === 6) {
+		setTime3(0);
+		setTime2(0);
+		setTime(0);
+		setClase1("circleR");
+		setClase2("circleY");
+		setClase3("circleGON");
+	}
+
+	function RedON() {
+		setClase1("circleRON");
+		setClase2("circleY");
+		setClase3("circleG");
+	}
+
+	function yellowON() {
+		setClase1("circleR");
+		setClase2("circleYON");
+		setClase3("circleG");
+	}
+	function greenON() {
+		setClase1("circleR");
+		setClase2("circleY");
+		setClase3("circleGON");
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="light">
+			<div className={clase1} onClick={() => RedON()}></div>
+			<div className={clase2} onClick={() => yellowON()}></div>
+			<div className={clase3} onClick={() => greenON()}></div>
 		</div>
 	);
 }
